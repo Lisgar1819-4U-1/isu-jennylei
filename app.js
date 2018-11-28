d50a786e5c9968bedb17691164166debcc155550
jlei <jenny.h.lei@hotmail.com>
2018-11-27 20:56:13 -0500

    Can create announcement and displays it by month

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ let cardContainer = document.getElementById('card-container');
 
 window.onload = function() {
 	monthClick();
-	createAnnouncement('lololo', 4, '1');
-	createAnnouncement('lololo', 5, '1');
+	createAnnouncement('lololo', 4, 1);
+	createAnnouncement('lololo', 5, 1);
+	createAnnouncement('lololo2', 5, 1);
+	createAnnouncement('lololo2', 5, 2);
 	// createAnnouncement('lololo', 6, '1');
 }
 
@@ -19,11 +21,20 @@ function monthClick() {
 			this.classList.add("selected");
 
 			mainContent.getElementsByTagName("h3")[0].innerText = this.innerText;
+
+			for	(let j = 0; j < cardContainer.children.length; j++) {
+				cardContainer.children[j].style.display = 'block';
+
+				if (cardContainer.children[j].classList[2] != i + 1) {
+					cardContainer.children[j].style.display = 'none';
+				}
+			}
 		}
 	}
 }
 
 function createAnnouncement(title, date, month) {
+	console.log(date);
 	let index = 0;
 	let exists = false;
 
@@ -31,26 +42,26 @@ function createAnnouncement(title, date, month) {
 		exists = false;
 	} else {
 		for (let i = 0; i < cardContainer.children.length; i++) {
-			if (cardContainer.children[i].classList[2] == date) {
+			if (cardContainer.children[i].classList[1] == date && cardContainer.children[i].classList[2] == month) {
 				index = i;
 				exists = true;
+				console.log(date + ': ' + index);
 			}
 		}
 	}
 
 	if (!exists) {
-		createCard(date, month);
-		index = cardContainer.children.length;
-		console.log(cardContainer.children.length);
+		createDate(date, month);
+		index = cardContainer.children.length - 1;
 	}
 
-	console.log(index);
+	console.log(date + ": " + index);
 	createCard(index, title, date, month);
 }
 
 function createDate(date, month) {
 	cardContainer.innerHTML += `
-		<div class="dates ${month} ${date}">
+		<div class="dates ${date} ${month}">
 			<h4 class="date">${date}</h4>
 		</div>`;
 }
