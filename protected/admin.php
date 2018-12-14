2b1afc96be4f59994638fc3bd8f151c47f51a9f7
jlei <jenny.h.lei@hotmail.com>
2018-12-14 17:52:00 -0500

    href logout

--- protected/admin.php
+++ protected/admin.php
@@ -20,7 +20,7 @@
 			<div class="float-right">
 				<p class="header-btn create" id="create" onclick="location.href='create.php'">Create</p>
 				<!-- <p class="header-btn settings" id="settings">Settings</p> -->
-				<a class="header-btn log-out" id="log-out" onclick="/logout">Log out</a>
+				<a class="header-btn log-out" id="log-out" href="/logout">Log out</a>
 			</div>
 		</div>
 
